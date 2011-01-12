9ce3f2db4d0a13469272cf7ce0ec8bb4703fcbdf
Carlos R. Mederos Arias <krlosmederos@gmail.com>
2011-01-12 04:32:59 +0100

    change models

---
+++ BasicWMS.Models/Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWMS.Models
+{
+    public class Stock : IModelBase
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public Guid UniqueKey { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public virtual ICollection<Order> Orders  { get; set; }
+    }
+}
