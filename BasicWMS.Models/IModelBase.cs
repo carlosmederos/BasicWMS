8e589d3d15cfca2aee3f850f79fc4cdd68856385
Carlos R. Mederos Arias <krlosmederos@gmail.com>
2011-01-12 03:57:13 +0100

    modelUser

---
+++ BasicWMS.Models/IModelBase.cs
@@ -0,0 +1,7 @@
+﻿namespace BasicWMS.Models
+{
+    public interface IModelBase
+    {
+        int Id { get; set; } 
+    }
+}
---
+++ BasicWMS.Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWMS.Models
+{
+    public class Role : IModelBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
