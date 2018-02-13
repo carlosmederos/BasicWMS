2e4c4ae4f3b730af7ad1f4dd7bc3e37a44b7ac7e
Carlos R. Mederos Arias <krlosmederos@gmail.com>
2018-02-13 13:56:44 +0100

    Merge branch 'feature/data' into develop

---
+++ BasicWMS.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWMS.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory _dbFactory;
+        private BasicWmsContext _dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public BasicWmsContext DbContext
+        {
+            get { return _dbContext ?? (_dbContext = _dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.Commit();
+        }
+    }
+}
