19593f8d29108e0c545f7943103eb889ecf27bae
Carlos R. Mederos Arias <krlosmederos@gmail.com>
2018-02-13 11:59:05 +0100

    EntitiesConfigurations

---
+++ BasicWMS.Data/Configurations/ModelBaseConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using BasicWMS.Models;
+
+namespace BasicWMS.Data.Configurations
+{
+    public class ModelBaseConfiguration<T> : EntityTypeConfiguration<T> where T : class, IModelBase
+    {
+        public ModelBaseConfiguration()
+        {
+            HasKey(e => e.Id);
+        }
+    }
+}
---
+++ BasicWMS.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicWMS.Models;
+
+namespace BasicWMS.Data.Configurations
+{
+    public class ProductConfiguration : ModelBaseConfiguration<Product>
+    {
+        public ProductConfiguration()
+        {
+            Property(p => p.Name).IsRequired().HasMaxLength(50);
+            Property(p => p.InventoryOnHand).IsRequired().HasColumnName("Inventory on Hand");
+            Property(p => p.InventoryReceived).IsRequired().HasColumnName("InventoryReceived");
+            Property(p => p.InventoryShipped).IsRequired().HasColumnName("Inventory Shipped");
+            Property(p => p.Label).IsOptional();
+            Property(p => p.MinimumRequired).IsRequired().HasColumnName("Minimum Required");
+            Property(p => p.Description).IsOptional().HasMaxLength(100);
+            Property(p => p.StartingInventory).IsRequired().HasColumnName("StartingInventory");
+
+            HasMany(p => p.Stocks).WithRequired().HasForeignKey(s => s.ProductId);
+        }
+    }
+}
