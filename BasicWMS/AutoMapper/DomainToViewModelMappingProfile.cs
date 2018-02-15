144d17ac7f7c263e93e317c317e45c345e173429
Carlos René Mederos Arias <carlosmederos@yandex.com>
2018-02-15 16:28:59 -0500

    automapper_config2

---
+++ BasicWMS/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace BasicWMS.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+    }
+}
