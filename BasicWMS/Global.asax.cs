0106cbf31dfeedd5739831f9e15faddf8855f8a9
Carlos R. Mederos Arias <krlosmederos@gmail.com>
2018-02-17 15:36:00 +0100

    configurations

--- BasicWMS/Global.asax.cs
+++ BasicWMS/Global.asax.cs
@@ -23,7 +23,6 @@ namespace BasicWMS
 
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
