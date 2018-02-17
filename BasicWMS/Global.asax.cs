4a94e499870c9273ede0b8ea8aff7f72c2a6fb19
Carlos R. Mederos Arias <krlosmederos@gmail.com>
2018-02-17 15:39:51 +0100

    Merge branch 'feature/initAutoFac' into develop

--- BasicWMS/Global.asax.cs
+++ BasicWMS/Global.asax.cs
@@ -23,11 +23,11 @@ namespace BasicWMS
 
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
             Bootstrapper.Run();
         }
     }
