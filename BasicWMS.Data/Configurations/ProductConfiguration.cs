54f82e56c9ba891579744e07095eb1d94b229ef1
Carlos René Mederos Arias <carlosmederos@yandex.com>
2018-04-22 20:52:51 -0300

    gitflow-release-stash: v2.0

---
+++ BasicWMS.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicWMS.Model;
+using System.Data.Entity.ModelConfiguration;
+
+namespace BasicWMS.Data.Configurations
+{
+    public class ProductConfiguration : EntityTypeConfiguration<Product>
+    {
+        public ProductConfiguration()
+        {
+            ToTable("Products");
+            HasKey(p => p.Id);
+            Property(p => p.Codigo).IsRequired().HasMaxLength(50);
+            Property(p => p.Nombre).IsRequired().HasMaxLength(30);
+            Property(p => p.CantidadDisponible).IsRequired();
+            Property(p => p.CantidadMinima);
+            Property(p => p.Descripcion);
+            Property(p => p.Disponible);
+        }
+    }
+}
---
+++ BasicWMS.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicWMS.Data.Infrastructure;
+using BasicWMS.Data.Repositories;
+using BasicWMS.Model;
+using BasicWMS.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BasicWMS.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        
+
+    }
+}
